bc7fdddc659d605b81a469a068cf310e19398bad
Barbara Stadnik <barbara1stadnik@gmail.com>
2018-02-20 11:44:34 +0100

    build mechanism for adding new list items

---
+++ assets/js/todos.js
@@ -0,0 +1,20 @@
+//check off specific todos by clicking
+$("ul").on("click", "li", function() {
+	$(this).toggleClass("completed");
+});
+
+//click on X to delete todo
+$("ul").on("click", "li span", function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		var todoText = $(this).val();
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+		$(this).val("");
+	}
+});
